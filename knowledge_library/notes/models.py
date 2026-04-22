054987562a40f66e687d04b65882fdf768936a9d
AliBugged <alikhan.assilbek@gmail.com>
2026-04-23 00:35:35 +0500

    Add user auth and note ownership

--- knowledge_library/notes/models.py
+++ knowledge_library/notes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -11,6 +11,7 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
